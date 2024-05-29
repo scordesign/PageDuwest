f85dad6e7001506da267d3d98e1090b1e86f31e6
Seb1995 <99344424+scordesign@users.noreply.github.com>
2024-05-29 16:15:20 -0500

    popup red nacional

---
+++ js/script22.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var popupLinks = document.querySelectorAll('.popup-link1');
+    var popupContainer1 = document.querySelector('.popup-container1');
+    var popupTitle1 = document.getElementById('popup-title1');
+    var popupTitle1 = document.getElementById('popup-subtitle1');
+    var popupText1 = document.getElementById('popup-text1');
+    var closeBtn = document.querySelector('.close-btn1');
+
+    popupLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var title1 = link.getAttribute('data-title1');
+            var subtitle1 = link.getAttribute('data-subtitle1');
+            var text1 = link.getAttribute('data-text1');
+            document.getElementById('popup-title1').textContent = title1;
+            document.getElementById('popup-subtitle1').textContent = subtitle1;
+            document.getElementById('popup-text1').textContent = text1;
+            popupContainer1.style.display = 'block';
+        });
+    });
+
+    closeBtn.addEventListener('click', function() {
+        popupContainer1.style.display = 'none';
+        // Mostrar la sección #tm-section-10 al cerrar el popup
+        $("#tm-section-11").fadeIn(1000);
+        // Ocultar las demás secciones si es necesario
+        $(".tm-section").not("#tm-section-11").hide();
+        // Desplazar la página hacia la #tm-section-10
+        currentPageID = "#tm-section-11";
+        console.log(currentPageID+'<<<<<<<<<<<<<<<<');
+        $("html, body").animate({
+            scrollTop: $("#tm-section-11").offset().top
+        }, 1000);
+    });
+
+    popupContainer1.addEventListener('click', function(event) {
+        // Evitar que el clic en el popup se propague al contenedor
+        event.stopPropagation();
+    });
+});
